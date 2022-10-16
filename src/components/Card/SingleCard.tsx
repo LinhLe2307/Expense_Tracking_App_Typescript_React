8af8c9e97561014ee4575b6d6fb1b54d8384fa6c
Linh Le <lemylinh23071997@gmail.com>
2022-10-16 14:13:29 +0300

    Added color

--- src/components/Card/SingleCard.tsx
+++ src/components/Card/SingleCard.tsx
@@ -13,6 +13,7 @@ const SingleCard = ({expense}: MyProps) => {
   return (
     <Card>
         <Card.Body>
+            <div style={{backgroundColor: `${expense.color}`, width: "5rem", height: "5rem"}}></div>
             <Card.Title>Title {expense.title}</Card.Title>
             <Card.Text>Price {expense.price}</Card.Text>
         </Card.Body>
