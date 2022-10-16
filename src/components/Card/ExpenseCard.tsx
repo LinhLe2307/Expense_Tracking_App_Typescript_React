c0c0a9cc2a80be55957e526b5cc3bd8692dafd69
Linh Le <lemylinh23071997@gmail.com>
2022-10-16 03:42:51 +0300

    Added axios

--- src/components/Card/ExpenseCard.tsx
+++ src/components/Card/ExpenseCard.tsx
@@ -3,32 +3,30 @@ import Button from 'react-bootstrap/Button';
 
 interface MyProps {
   handleAddExpense: () => void,
-  handleInputExpense: (e:React.ChangeEvent<HTMLFormElement>) => void
+  handleInputExpense: (e: React.ChangeEvent<HTMLInputElement>) => void
+  addSubmitHandler: (e: React.ChangeEvent<HTMLFormElement>)=>void
 }
 
-function ExpenseCard ({handleAddExpense, handleInputExpense}:MyProps ){
+function ExpenseCard ({handleAddExpense, handleInputExpense, addSubmitHandler}:MyProps ){
   return (
     <Card>
       <Card.Body>
         <Card.Title>Add Expense</Card.Title>
-        <form onSubmit={handleInputExpense}>
+        <form onSubmit={addSubmitHandler}>
           <div>
             <label>Title</label>
-            <input name="title"/>
+            <input required name="title" type="text" onChange={handleInputExpense}/>
           </div>
           <div>
             <label>Price</label>
-            <input name="price"/>
+            <input required name="price"  type="text" onChange={handleInputExpense}/>
           </div>
           <div>
             <label>Categories</label>
-            <div>
-
-            </div>
           </div>
           <div>
             <label>Color</label>
-            <input type="color" name="color"/>
+            <input required type="color" name="color" onChange={handleInputExpense}/>
           </div>
           <Button variant="primary" onClick={handleAddExpense} type="submit">Save</Button>
         </form>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,7 @@ import SingleCard from "./Card/SingleCard";
 import Button from 'react-bootstrap/Button';
 import { useState } from "react";
 import ExpenseCard from "./Card/ExpenseCard";
+import axios from "axios"
 
 interface InputExpense {
     title: string,
@@ -21,23 +22,35 @@ const HomePage = () => {
         setAddExpense(prev => !prev)
     }
 
-    const handleInputExpense = (e: React.ChangeEvent<HTMLFormElement> ): void => {
-        setInputExpense({
+    const handleInputExpense = (e: React.ChangeEvent<HTMLInputElement> ): void => {
+        setInputExpense(()=> {
+            return {
             ...inputExpense,
             [e.target.name] : e.target.value
-        })
-        console.log(inputExpense.title)
+        }})
+    }
+
+    const addSubmitHandler = (e: React.ChangeEvent<HTMLFormElement>):void => {
+        e.preventDefault()
+        axios.post("http://localhost:3010/notes", inputExpense)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
     }
 
   return (
     <div>
-        <div>{inputExpense.title}</div>
-        <div>{inputExpense.color}</div>
         {/* <SingleCard /> */}
         <Button variant="primary" onClick={handleAddExpense}>+</Button>
-        {addExpense && <ExpenseCard handleAddExpense={handleAddExpense}
-        handleInputExpense={handleInputExpense}
-        />}
+        <div>{inputExpense.title}</div>
+        <div>{inputExpense.color}</div>
+        <div>{inputExpense.price}</div>
+        {addExpense && 
+            <ExpenseCard 
+                handleAddExpense={handleAddExpense}
+                handleInputExpense={handleInputExpense}
+                addSubmitHandler={addSubmitHandler}
+            />
+        }
     </div>
   )
 }
