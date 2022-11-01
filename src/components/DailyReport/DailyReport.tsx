9393d751fcb84365fc52800de8562c1463f908f8
Linh Le <lemylinh23071997@gmail.com>
2022-11-01 23:52:36 +0200

    merge

--- src/components/DailyReport/DailyReport.tsx
+++ src/components/DailyReport/DailyReport.tsx
@@ -1,12 +1,36 @@
 import { nanoid } from 'nanoid';
 import React, {useState, useEffect} from 'react';
-import { Calendar } from 'react-calendar';
+// import { Calendar } from 'react-calendar';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { initializeExpense } from '../../features/expense/expenseSlice';
 import { customDate } from '../../functions/reusableFunction';
 import SingleCard from '../Card/SingleCard';
 import { ExpenseModel } from '../../models/reduxModels';
-import 'react-calendar/dist/Calendar.css';
+// import 'react-calendar/dist/Calendar.css';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import "./calendar.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const onSelectSlot = (pressedDate) => {
+     console.log("pressed Date: ", pressedDate)
+  };
 
 const DailyReport = () => {
     const [value, onChange] = useState(new Date());
@@ -19,13 +43,15 @@ const DailyReport = () => {
 
   return (
     <div>
-        <Calendar 
+        {/* <Calendar 
             onChange={onChange} 
             value={value} 
             // onClickDay={(value, event) => console.log('Clicked month: ', value)} 
             showDoubleView={true}
             
-        />
+        /> */}
+        <Calendar localizer={localizer} selectable={true} startAccessor="start" onSelectSlot={onSelectSlot} endAccessor="end" style={{ height: 500, margin: "50px" }} />
+
         <h3>{customDate(value)}</h3>
         {
             expenseLists
